7d3321562ef61c167a5e28944d8a67fa5657d650
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-06-06 10:29:34 -0300

    Update cripto_module..vhd

--- cripto_module..vhd
+++ cripto_module..vhd
@@ -31,7 +31,7 @@ END ENTITY;
 
 ARCHITECTURE cripto_module OF cripto_module IS
 
-    TYPE STATE IS (IDLE, E2, E3, E4, E5, E6. E7, E8, E9, E10, E11, E12, E13, E14, E15, E16, E17, E18, E19, E20, E21); -- Etapas da máquina de estados   N SEI SE PRECISA DE RESET OU N AINDA
+    TYPE STATE IS (IDLE, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12, E13, E14, E15, E16, E17, E18, E19, E20, E21); -- Etapas da máquina de estados   N SEI SE PRECISA DE RESET OU N AINDA
     SIGNAL EA : state; -- Estado atual
     SIGNAL EF : state; -- Estado futuro 
     SIGNAL for_num : STD_LOGIC := '0'; --- quando for 0 é o primeiro e qnd for ''1' é o segundo o acabou
